a1a9770ac8943aed9727d42efca79580bfd5a143
Olivier_Luethy <olivier@kauz.ch>
2022-12-22 08:38:00 +0100

    Code refactoring made by chat gpt

--- mark_calculator.py
+++ mark_calculator.py
@@ -1,11 +1,14 @@
-# The points you've earned
+# Prompt the user to enter the number of points they have achieved
 achievedPoints = int(input("How many points did you get?\t\t"))
 
-# The maximum points you can actually get
+# Prompt the user to enter the maximum number of points that can be achieved
 maxPoints = int(input("What are the max points you can get?\t\t"))
 
-# Calculates your mark using formula
+# Calculate the student's mark using the formula
 yourMark = ((achievedPoints * 5) / maxPoints) + 1
 
-# Output of calculated mark
-print(f"You have the mark of {yourMark:.2f}")
+# Round the mark to 2 decimal places
+yourMark = round(yourMark, 2)
+
+# Print the student's mark
+print(f"You have the mark of {yourMark}")
